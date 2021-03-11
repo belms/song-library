ad3b780cf4f9c03a93ef9ad321e00c9439acd3d6
Belma Jakupovic <jakupovicbelma1@gmail.com>
2021-03-11 17:54:43 +0100

    initial commit

---
+++ src/main/java/com/iste610/SongLibrary/SongLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.iste610.SongLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iste610/SongLibrary/SongLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iste610.SongLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
